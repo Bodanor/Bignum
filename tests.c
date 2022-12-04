3bb6f3ac13c7a80b0010d34dcd9e5deb52990a68
Bodanor <christos.papadopoulos@student.hepl.be>
2022-12-04 18:43:47 +0100

    Added starnger behaviour that needs a fix

--- tests.c
+++ tests.c
@@ -30,12 +30,25 @@ void test_addBigger(void)
     res = addBigNum(num1, num2);
     assert_string_equal("24769465548784193629945629100230557966192499780204797629155780204801229296", BigNumToStr(res));
 }
+void test_firstLower(void)
+{
+    bignum *num1;
+    bignum *num2;
+    bignum *res;
 
+    num1 = BigNum("317828903712893718927389127"); 
+    num2 = BigNum("7318927398172893712897389127389127389712893712893789127389127389712893721");
+
+    res = addBigNum(num1, num2);
+    assert_string_equal("7318927398172893712897389127389127389712893713211618031102021108640282848", BigNumToStr(res));
+
+}
 void test_fixture_add(void)
 {
     test_fixture_start();      
     run_test(test_add);   
     run_test(test_addBigger);   
+    run_test(test_firstLower);
     test_fixture_end();    
 }
 void all_tests(void)
