21c59028e27aafb2a5f725f0f7b366198a913081
Bodanor <christos.papadopoulos@student.hepl.be>
2022-12-04 18:35:57 +0100

    Added unit-tests

--- makefile
+++ makefile
@@ -1,13 +1,17 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -D DEBUG -g
 
-C_SOURCES := $(wildcard *.c)
-C_HEADERS := $(wildcard *.h)
+C_SOURCES := $(filter-out seatest.c tests.c, $(wildcard *.c))
+C_HEADERS := $(filter-out seatest.h, $(wildcard *.h))
 C_OBJECTS := $(C_SOURCES:.c=.o)
 
 bignum: $(C_OBJECTS) $(C_SOURCES) $(C_HEADERS)
 	$(info Building target : $<)
 	$(CC) $(CFLAGS) -o $@ $(C_OBJECTS)
+
+tests: $(filter-out main.c, $(C_SOURCES)) $(filter-out main.o, $(C_OBJECTS)) seatest.c seatest.h tests.c
+	$(info Making tests...)
+	$(CC) $(CFLAGS) -o $@ seatest.c $(filter-out main.o, $(C_OBJECTS)) tests.c
 
 %.o: %.c
 	$(info Building : $<)
@@ -16,3 +20,4 @@ bignum: $(C_OBJECTS) $(C_SOURCES) $(C_HEADERS)
 clean:
 	rm -rf *.o
 	rm -rf bignum
+	rm -rf tests
---
+++ tests.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "bignum.h"
+#include "util.h"
+
+#include "seatest.h"
+
+void test_add(void)
+{
+    bignum *num1;
+    bignum *num2;
+    bignum *res;
+
+    num1 = BigNum("12");
+    num2 = BigNum("12");
+
+    res = addBigNum(num1, num2);
+    assert_string_equal("24", BigNumToStr(res));
+}
+void test_addBigger(void)
+{
+    bignum *num1;
+    bignum *num2;
+    bignum *res;
+
+    num1 = BigNum("12378187238971289739817238971289738917289371289371892738917289371892738912");
+    num2 = BigNum("12391278309812903890128390128940819048903128490832904890238490832908490384");
+
+    res = addBigNum(num1, num2);
+    assert_string_equal("24769465548784193629945629100230557966192499780204797629155780204801229296", BigNumToStr(res));
+}
+
+void test_fixture_add(void)
+{
+    test_fixture_start();      
+    run_test(test_add);   
+    run_test(test_addBigger);   
+    test_fixture_end();    
+}
+void all_tests(void)
+{
+    test_fixture_add();
+}
+
+
+int main()
+{
+   return run_tests(all_tests);
+    
+
+}
